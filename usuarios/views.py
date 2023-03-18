dce17c16a99eea8150a2902cf37e90e9e0877dfc
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-03-18 16:08:09 -0400

    Terminado a função  valida_cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Usuario
+from django.shortcuts import redirect
+from hashlib import sha256
 
 
 def login(request):
@@ -19,5 +21,37 @@ def valida_cadastro(request):
    nome = request.POST.get('nome')
    email = request.POST.get('email')
    senha = request.POST.get('senha')
-   return HttpResponse(f"{nome} {email} {senha}")
+   
+   # Verificando se  nome e email não é vazio
+   if len(nome.strip()) == 0 or len(email.strip()) == 0:
+      return redirect('auth/login/?status=1')
+   
+   # Verificando senha senha não é menor que 8 
+   if len(senha) < 8:
+      return redirect('auth/login/?status=2')
+      
+      
+   # Buscando no banco bando se o email digitado no cadastro ja não tem no banco 
+   usuario = Usuario.objects.filter(email = email)
 
+   # Verificando se existe email igual ao digitado no cadastro
+   if len(usuario) > 0:
+      return redirect('auth/login/?status=3')
+   
+   
+   try:
+      # criptografando a senha 
+      senha_criptografada = sha256(senha.encode()).hexdigest()   
+      # Instanciando a usuario
+      usuario = usuario(nome=nome, email=email, senha=senha_criptografada)   
+      # Salvando no bando de dados
+      usuario.save()      
+      return redirect('auth/login/?status=0')
+   
+   except:
+        return redirect('auth/login/?status=4')
+
+
+   
+  
+   
