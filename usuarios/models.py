d48e04891a191d1e5edf9623694804dd05da612e
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-03-18 14:42:14 -0400

    Criado o primeiro model usuarios e feito migrate

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class usuario(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    senha = models.CharField(max_length=100)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
-# Create your views here.
+
+def login(request):
+   return render(request, 'login.html')
+  
+
+
+
+def cadastro(requests):
+    return render(requests, 'cadastro.html')
+
+
+
