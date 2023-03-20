5feeff17076e92ea0a421668bd1d6b194c37c7b3
Rogério Maia <rogerioreservadosinop@gmail.com>
2023-03-20 11:27:55 -0400

    Terminado sistema de login sem session

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,56 +2,69 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Usuario
 from django.shortcuts import redirect
+from django.urls import reverse
 from hashlib import sha256
 
 
 def login(request):
-   return render(request, 'login.html')
-  
+    status = request.GET.get('status')
+    return render(request, 'login.html', {'status':status})
+
+
+def valida_login(request):
+    email = request.POST.get('email')   
+    senha = request.POST.get('senha')
+    senha = sha256(senha.encode()).hexdigest()
+
+    usuario = Usuario.objects.filter(email = email).filter(senha=senha)
+
+    if len(usuario) == 0:
+        return redirect(reverse('login') + '?status=0')
+    elif len(usuario) > 0:
+         pass 
+    
+    
+        
+
+    
+
+    
+    
+    
+    
+    
+
+    
+    
 
 
 
-def cadastro(requests):
-    return render(requests, 'cadastro.html')
 
 
+def cadastro(request):
+    status = request.GET.get('status')
+    return render(request, 'cadastro.html', {'status': status})
 
 def valida_cadastro(request):
-   
-   nome = request.POST.get('nome')
-   email = request.POST.get('email')
-   senha = request.POST.get('senha')
-   
-   # Verificando se  nome e email não é vazio
-   if len(nome.strip()) == 0 or len(email.strip()) == 0:
-      return redirect('auth/login/?status=1')
-   
-   # Verificando senha senha não é menor que 8 
-   if len(senha) < 8:
-      return redirect('auth/login/?status=2')
-      
-      
-   # Buscando no banco bando se o email digitado no cadastro ja não tem no banco 
-   usuario = Usuario.objects.filter(email = email)
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
 
-   # Verificando se existe email igual ao digitado no cadastro
-   if len(usuario) > 0:
-      return redirect('auth/login/?status=3')
-   
-   
-   try:
-      # criptografando a senha 
-      senha_criptografada = sha256(senha.encode()).hexdigest()   
-      # Instanciando a usuario
-      usuario = Usuario(nome=nome, email=email, senha=senha_criptografada)   
-      # Salvando no bando de dados
-      usuario.save()      
-      return redirect('auth/login/?status=0')
-   
-   except:
-        return redirect('auth/login/?status=4')
+    if len(nome.strip()) == 0 or len(email.strip()) == 0:
+        return redirect(reverse('cadastro') + '?status=1')
 
+    if len(senha) < 8:
+        return redirect(reverse('cadastro') + '?status=2')
 
-   
-  
-   
+    usuario = Usuario.objects.filter(email = email)
+
+    if len(usuario) > 0:
+        return redirect(reverse('cadastro') + '?status=3')
+
+    try:
+        senha_criptografada = sha256(senha.encode()).hexdigest()
+        usuario = Usuario(nome=nome, email=email, senha=senha_criptografada)
+        usuario.save()
+        return redirect(reverse('cadastro') + '?status=0')
+    except:
+        return redirect(reverse('cadastro') + '?status=4')
